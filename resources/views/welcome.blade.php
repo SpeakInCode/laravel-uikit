ca20f4ce0e9c57d243384cd3af04a38037e86e96
“SpeakInCode” <speakincode6@gmail.com>
2019-03-04 20:08:05 -0700

    Latest

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,6 @@
 
 @section('content')
 
-  <div class="uk-container">
-    <h1 class="uk-text-center">Home</h1>
-
-    <div class="uk-child-width-expand uk-grid-small" uk-grid uk-sortable>
-      <div>
-        <div class="uk-card uk-card-default uk-card-body">Item 1</div>
-      </div>
-      <div>
-        <div class="uk-card uk-card-default uk-card-body">Item 2</div>
-      </div>
-      <div>
-        <div class="uk-card uk-card-default uk-card-body">Item 3</div>
-      </div>
-    </div>
-
-  </div>
+  <h1 class="uk-text-center">Welcome</h1>
 
 @endsection
